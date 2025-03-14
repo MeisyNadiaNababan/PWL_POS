5e52bef484d949e1125358b0a4d29f086ab838dd
MeisyNadiaNababan <meisynadianababan@gmail.com>
2025-03-15 04:26:02 +0700

    JS04_P2.7_L2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,7 +56,7 @@ class UserController extends Controller
 
     public function index() {
         $user = UserModel::with('level')->get();
-        return view('user', ['data' => $user]);
+        dd($user);
     }
 
 
